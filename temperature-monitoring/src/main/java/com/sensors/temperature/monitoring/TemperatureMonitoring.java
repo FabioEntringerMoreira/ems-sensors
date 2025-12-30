3f61a170893f183b40bb6e3dfa3c640073450e92
Fabio <fabioentringer@gmail.com>
2025-12-30 23:59:03 +0100

    Configure project structure

---
+++ temperature-monitoring/src/main/java/com/sensors/temperature/monitoring/TemperatureMonitoring.java
@@ -0,0 +1,13 @@
+package com.sensors.temperature.monitoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureMonitoring {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureMonitoring.class, args);
+    }
+
+}
---
+++ temperature-processing/src/main/java/com.sensors.temperature.processing/TemperatureProcessing.java
@@ -0,0 +1,13 @@
+package com.sensors.temperature.processing;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemperatureProcessing {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TemperatureProcessing.class, args);
+    }
+
+}
